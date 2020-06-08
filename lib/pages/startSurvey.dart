ea73c149c22e3eaa9155f5bd6541ca3fe3162570
ShuiX <shajith.307@gmail.com>
2020-06-08 18:51:47 +0200

    Update startSurvey.dart

--- lib/pages/startSurvey.dart
+++ lib/pages/startSurvey.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:va_flutter_project/modules/firebaseApp.dart';
 import 'package:va_flutter_project/modules/surveyCard.dart';
-import 'package:va_flutter_project/pages/endingSurvey.dart';
+
 
 class StartSurvey extends StatefulWidget {
   final String code;
@@ -9,16 +9,11 @@ class StartSurvey extends StatefulWidget {
 
   StartSurvey({this.code, this.surveyData});
 
-
   @override
   _StartSurveyState createState() => _StartSurveyState();
 }
 
 class _StartSurveyState extends State<StartSurvey> {
-  List<Map> tempSave = [];
-  List<Widget> surveyCards = [];
-  String _currentOptionValue;
-  String _followroute;
 
   @override
   void initState() {
@@ -31,31 +26,12 @@ class _StartSurveyState extends State<StartSurvey> {
   dynamic inputOptions(
       BuildContext context, String type, Map data, String route) {
     switch (type) {
-      case 'selection':
-        return selectionOption(context, data, route);
-        break;
-      case 'sentence':
-        return sentenceOption(context, data, route);
-        break;
       case 'info':
         return contentOption(context, data, route);
         break;
       default:
         break;
     }
-  }
-
-  void finishSurvey(BuildContext context, Map data) {
-    Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) => EndingSurvey(
-                endingDescription: data["endingdescription"],
-                results: tempSave,
-                code: widget.code,
-              )),
-    );
   }
 
   contentOption(BuildContext context, Map data, String route) {
@@ -70,68 +46,6 @@ class _StartSurveyState extends State<StartSurvey> {
       ),
     );
     return textContent;
-  }
-
-  sentenceOption(BuildContext context, Map data, String route) {
-    var sentenceOptionText = TextEditingController();
-    _followroute = data[route]["reply"]["followroute"];
-    List<Widget> sentence = new List<Widget>();
-    sentence.add(TextField(
-      controller: sentenceOptionText,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Enter your Answer',
-        labelStyle: TextStyle(
-          color: Colors.grey,
-        ),
-      ),
-      style: TextStyle(fontSize: 20, color: Colors.black),
-      onChanged: (textValue) {
-        _currentOptionValue = textValue;
-        _followroute = data[route]["reply"]["followroute"];
-      },
-      onSubmitted: (textValue) {
-        _currentOptionValue = textValue;
-        tempSave.add(
-          {
-            "value": _currentOptionValue,
-            "followroute": _followroute,
-            "route": route,
-            "type": data[route]["type"]
-          },
-        );
-        if (_followroute == "finish") {
-          finishSurvey(context, {});
-        } else {
-          setState(() {});
-        }
-      },
-    ));
-    return sentence;
-  }
-
-  selectionOption(BuildContext context, Map data, String route) {
-    List<Widget> optionList = new List<Widget>();
-    for (var item in data[route]["reply"]) {
-      setState(() {
-        optionList.add(new RadioListTile(
-          title: Text(
-            item["selection"],
-            style: TextStyle(color: Colors.black),
-          ),
-          value: item["selection"],
-          groupValue: _currentOptionValue,
-          onChanged: (value) {
-            this.setState(() {
-              _currentOptionValue = value;
-              _followroute = item["followroute"];
-              surveyCards.removeLast();
-            });
-          },
-        ));
-      });
-    }
-    return optionList;
   }
 
   Widget _startCard(double titleSize, double subtitleSize, double textSize,
@@ -212,7 +126,7 @@ class _StartSurveyState extends State<StartSurvey> {
                       pageBuilder: (BuildContext context, _, __) => SurveyCard(
                         "start",
                         widget.code,
-                        widget.surveyData ?? {},
+                        widget.surveyData ?? {"lastRoute": {}, "history": {}},
                         data["title"],
                       ),
                       opaque: false,
